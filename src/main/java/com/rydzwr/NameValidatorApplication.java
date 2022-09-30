94863357184a0ee15d76d1ed2cc536293fd17fba
rydzwr <rydzwr@gmail.com>
2022-09-30 17:57:37 +0200

    Build Project

---
+++ src/main/java/com/rydzwr/NameValidatorApplication.java
@@ -0,0 +1,13 @@
+package com.rydzwr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NameValidatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NameValidatorApplication.class, args);
+	}
+
+}
